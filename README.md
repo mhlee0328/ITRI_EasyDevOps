3768f9781cc5a698f5c3202c0113efcac29a4fdc
mhlee0328 <mhlee0911779731@gmail.com>
2023-07-27 16:06:39 +0800

    Init: 容器微服務專案初始化

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # ITRI_EasyDevOps
+
+1. ### 描述：
+
+   ITRI_EasyDevOps 專案的主要用於 CI/CD DevOps 的容器化工具集。容器化有助於方便部署和管理。目標是讓開發團隊能夠專注於開發本身，而不需花費過多時間處理""環境設置和部署的瑣碎事務""。透過容器化，我們致力於提供一個方便且快速的開發環境，讓 CICD DevOps 變得更加順暢。
+
+2. ### 功能：
+
+   - SonarQube：程式碼靜態分析工具，幫助確保程式碼品質，發現潛在的程式碼問題。
+   - Jenkins：流程自動化工具，用於建置、測試和部署程式碼。
+   - PostgreSQL：強大的開源關聯式資料庫，用於儲存和管理專案所需的資料。
+
+3. ### SonarQube ( image: sonarqube:9.9-community )
+
+   image: sonarqube:9.9-community  
+
+   LTS SONARQUBE_VERSION=9.9.1.69595 
+
+   提供即時的程式碼品質反饋，確保開發團隊可以持續提供高品質的程式碼。
+
+4. ### Jenkins ( image: jenkins/jenkins:lts )
+
+   image: jenkins/jenkins:lts 2.401.2
+
+   負責自動化整個 CI/CD 流程，從程式碼的建置、測試，到最終的部署都能自動處理。
+
+5. ### PostgreSQL ( image: postgres:13 )
+
+   專案使用 PostgreSQL 作為可靠的資料庫，確保資料的儲存和管理是穩定且高效的。
+
+   
---
+++ YourScanFolder/YourAppProject/940115_RunSonarScanner-9.9LTS.ps1
@@ -0,0 +1,45 @@
+﻿## @echo off
+
+# 定義專案 utf8 繁體中文避免亂碼 , 必須存 UTF-8 with BOM
+# [console]::OutputEncoding = [System.Text.Encoding]::UTF8
+# 
+# https://blog.poychang.net/sonarqube-no-analysable-projects-were-found/
+
+Write-Host 
+Write-Host "========如果 dotnet sonarScanner 指令執行有異常========" -foregroundColor yellow
+Write-Host 
+Write-Host "---> 請確認本機上是否有安裝 dotnet sonarScanner 工具" -foregroundColor DarkMagenta
+Write-Host "安裝 Dotnet Core 指令 : dotnet tool install --global dotnet-sonarscanner"
+Write-Host 
+
+
+# 定義專案 key 與 專案名稱*.sln 或 .csproj
+$token="sqp_9aab436148de78ab0a19db2a2a2318392de4d171"
+$projectName="YourAppProject"
+$projectSlnPath=".\YourAppProject.sln"
+$sonarUsername="admin"  # 將 "Your_SonarQube_Username" 改為您的 SonarQube 使用者帳號
+$sonarPassword="@02750963"  # 將 "Your_SonarQube_Password" 改為您的 SonarQube 使用者密碼
+
+Write-Host "========開始進行 SonarScanner 程式碼掃描流程=========" -foregroundColor yellow
+Write-Host "當前執行路徑: "$TargetPath -foregroundColor blue
+Write-Host 
+
+Write-Host "========目前命令執行路徑========" -foregroundColor yellow
+Write-Host $PSScriptRoot
+Write-Host 
+
+Write-Host "========開始進行 程式碼掃瞄作業========" -foregroundColor yellow
+#dotnet sonarscanner begin /key:$projectName /d:sonar.host.url="http://localhost:9001"  /d:sonar.login=$token
+dotnet sonarscanner begin /key:$projectName /d:sonar.host.url="http://localhost:9001"  /d:sonar.login=$sonarUsername /d:sonar.password=$sonarPassword 
+
+Write-Host 
+Write-Host "========開始進行 專案建置作業========" -foregroundColor yellow
+dotnet build $projectSlnPath
+
+Write-Host
+Write-Host "========開始進行 程式碼掃瞄分析結果上傳作業========" -foregroundColor yellow
+#dotnet sonarscanner end /d:sonar.login=$token
+dotnet sonarscanner end /d:sonar.login=$sonarUsername /d:sonar.password=$sonarPassword 
+
+PAUSE
+# Read-Host -Prompt "Press any key to contine"
